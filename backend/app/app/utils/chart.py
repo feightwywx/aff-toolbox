f8fe119c62efc90440ebe2c5e7ad4886cee5f1f5
feightwywx <kururinmiracle@outlook.com>
2025-05-25 19:16:25 +0800

    add(frontend/chart-to-skyline): tap, hold, arc support

--- backend/app/app/utils/chart.py
+++ backend/app/app/utils/chart.py
@@ -47,3 +47,62 @@ def arc_break(arc: Arc, breakpoints: list[int], disp: float) -> NoteGroup:
             arc_current.toy -= y
 
     return arcs
+
+
+def note_to_skyline(note: a.Note) -> a.NoteGroup:
+    tap_duration = 25
+
+    def get_x_from_lane(lane: int) -> float:
+        return 0.5 * (lane - 1)
+    
+    print(note)
+    
+    if type(note) == a.Tap:
+        t1 = note.time
+        t2 = t1 + tap_duration
+        dx = get_x_from_lane(note.lane)
+        return NoteGroup(
+            [
+                Arc(t1, t1, -0.45 + dx, -0.05 + dx, "s", -0.20, -0.20, 0, True),
+                Arc(t1, t2, -0.45 + dx, -0.45 + dx, "s", -0.20, -0.20, 0, True),
+                Arc(t1, t2, -0.05 + dx, -0.05 + dx, "s", -0.20, -0.20, 0, True),
+                Arc(t2, t2, -0.45 + dx, -0.05 + dx, "s", -0.20, -0.20, 0, True),
+            ]
+        )
+    elif type(note) == a.Hold:
+        t1 = note.time
+        t2 = note.totime
+        dx = get_x_from_lane(note.lane)
+        return NoteGroup(
+            [
+                Arc(t1, t1, -0.45 + dx, -0.05 + dx, 's', -0.20, -0.20, 0, True),
+                Arc(t1, t2, -0.45 + dx, -0.45 + dx, 's', -0.20, -0.20, 0, True),
+                Arc(t1, t2, -0.05 + dx, -0.05 + dx, 's', -0.20, -0.20, 0, True),
+                Arc(t2, t2, -0.45 + dx, -0.05 + dx, 's', -0.20, -0.20, 0, True)
+            ]
+        )
+    elif type(note) == a.Arc and not note.isskyline:
+        t1 = note.time
+        t2 = note.totime
+        dx1 = note.fromx
+        dx2 = note.tox
+        dy1 = note.fromy
+        dy2 = note.toy
+        arc_type = note.slideeasing
+        color = note.color
+        return NoteGroup(
+            [
+                # start
+                Arc(t1, t1, -0.09 + dx1, 0.00 + dx1, 's', -0.07 + dy1, 0.09 + dy1, color, True),
+                Arc(t1, t1, 0.00 + dx1, 0.09 + dx1, 's', 0.09 + dy1, -0.07 + dy1, color, True),
+                # mid
+                Arc(t1, t2, -0.09 + dx1, -0.09 + dx2, arc_type, -0.07 + dy1, -0.07 + dy2, color, True),
+                Arc(t1, t2, 0.00 + dx1, 0.00 + dx2, arc_type, 0.09 + dy1, 0.09 + dy2, color, True),
+                Arc(t1, t2, 0.09 + dx1, 0.09 + dx2, arc_type, -0.07 + dy1, -0.07 + dy2, color, True),
+                # end
+                Arc(t2, t2, -0.09 + dx2, 0.00 + dx2, 's', -0.07 + dy2, 0.09 + dy2, color, True),
+                Arc(t2, t2, 0.00 + dx2, 0.09 + dx2, 's', 0.09 + dy2, -0.07 + dy2, color, True),
+            ]
+        )
+    else:
+        return NoteGroup([note])
